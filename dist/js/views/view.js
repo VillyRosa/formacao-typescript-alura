a5b9934876e2f4fd95e0215b7799ac36b431155a
VillyRosa <villyrosa@hotmail.com>
2024-04-23 17:59:54 -0300

    first commit

---
+++ dist/js/views/view.js
@@ -0,0 +1,9 @@
+export default class View {
+    constructor(seletor) {
+        this.elemento = document.querySelector(seletor);
+    }
+    update(model) {
+        const template = this.template(model);
+        this.elemento.innerHTML = template;
+    }
+}
